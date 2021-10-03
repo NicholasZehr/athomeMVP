908ac0a4d606168747da8bb9831a535c99b37623
Nicholas Zehr <nicholas.zehr@icloud.com>
2021-10-03 12:48:55 -0500

    Initialized FlutterFire per the docs firebase.flutter.dev/docs/overview/

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,32 @@
 // ignore: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart'
 
-void main() async => {await Firebase.initializeApp(), runApp(const MyApp())};
+void main() async => {await Firebase.initializeApp(), runApp(const App())};
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return MultiProvider(providers: [],
-    child: MaterialApp(
-        theme: ThemeData(primaryColor: Colors.purple[900]),
-        home: const AuthenticationWrapper())
-    );
-  }
+  _AppState createState() => _AppState();
 }
 
-class AuthenticationWrapper extends StatelessWidget {
-  const AuthenticationWrapper({Key? key}) : super(key: key);
+class _AppState extends State<App> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return SomethingWentWrong();
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MyApp();
+          }
+
+          return Loading();
+        });
   }
 }
