79410dfef5a809e680aec72bb1c6747212d22e83
Nicholas Zehr <nicholas.zehr@icloud.com>
2021-10-03 12:36:31 -0500

    Added FlutterFire to iOS Info.plist

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 // ignore: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
+import 'package:provider/provider.dart'
 
-void main() async => {await Firebase.initializeApp(), runApp(MyApp())};
+void main() async => {await Firebase.initializeApp(), runApp(const MyApp())};
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: null);
+    return MultiProvider(providers: [],
+    child: MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: const AuthenticationWrapper())
+    );
   }
 }
 
